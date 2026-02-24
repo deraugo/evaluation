752368ddbc1b2e77f349db895cb612b20ddc6a52
deraugo <chidera_chinedu@yahoo.com>
2026-02-24 12:24:32 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+geometry_sets = [
+    {"L1": 1.0, "L2": 0.8, "Mb": 2.0, "Mc": 1.5},
+    {"L1": 1.2, "L2": 1.0, "Mb": 1.5, "Mc": 2.0},
+]
+
+motion_sets = [
+    {"omega1": 4.0, "omega2": 3.0},
+    {"omega1": 6.0, "omega2": 5.0},
+]
